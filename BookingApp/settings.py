674e47d051d8e416b4a2aba8654ff24b1b12566f
Ubuntu <ubuntu@ip-172-31-16-218.ec2.internal>
2019-08-29 11:29:13 +0000

    Removed env dependency to allow Travis build the app

--- BookingApp/settings.py
+++ BookingApp/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import env
+# import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 ]
 
 MIDDLEWARE = [
